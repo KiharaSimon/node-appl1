53b37b368226b1d409893a9e8e82b7d517b799b0
simon <symokihara@gmail.com>
2021-04-12 20:34:50 +0300

    Introducing callbacks in the app

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var rect = require('./rectangle');
 
-var solveRect = (l, b) => {
-    console.log("calculating props of rectangles with Dimensions of " + l + " and " + b);
-
-    if (l <= 0 || b <= 0) {
-        console.log("Dimensions must be greater than zero")
-
-    }
-    else {
-        console.log("The area of the rectange of dimensions " + l + " width " + b + " is  " + rect.Area(l,b));
-        console.log("The perimeter of the rectange of dimensions " + l + " width " + b + " is " + rect.perimeter(l,b));
-    }
-
-
-}
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.Area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
 solveRect(10, 5);
-solveRect(-1, 20);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,15 @@
-exports.perimeter = (x, y) => (2*(x+y));
-    exports.Area = (x,y) => (x*y);
+module.exports = (x, y, cb) => {
+    if (x <= 0 || y <= 0) {
+       setTimeout(() => 
+       cb(new Error("Dimensions must be greater than zero"), null),
+       2000);
+    } 
+    else {
+        setTimeout(() => 
+        cb(null, {
+            perimeter: () => (2*(x + y)),
+            Area: () => (x * y)
+
+        }), 2000);
+    }
+}
